27d5511dbbac0c85877e736e069538a3c1ffb460
Bassachi <110608844+Bassachix@users.noreply.github.com>
2024-10-16 16:26:07 +0800

    Tugas Pertemuan 4
    Commit 2

---
+++ Pertemuan 4/disableWindow.py
@@ -0,0 +1,28 @@
+# Created by Khairul Fahmi_2210010241_5P
+
+from PyQt5.QtWidgets import QMainWindow, QWidget, QApplication, QPushButton, QLabel, QDesktopWidget
+from PyQt5 import QtCore
+
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.label = QLabel(self)
+        self.label.setText("Label1")
+        self.label.move(200, 0)
+        self.button = QPushButton(self)
+        self.button.setText("Button1")
+        self.setGeometry(0, 0, 500, 500)
+        self.setWindowTitle("Belajar PyQt5")
+        cwa = self.frameGeometry() # Cek ukuran main window app kita
+        cwc = QDesktopWidget().availableGeometry().center() # Pada screen saya: (682, 363)
+
+        #Print(cwc)
+        cwa.moveCenter(cwc)
+        self.move(cwa.topLeft())
+        self.setFixedSize(500, 500) # Agar tidak bisa di resize! icon maximize juga akan otomatis hilang
+        self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
+
+app = QApplication([])
+window = MyWindow()
+window.show()
+app.exec_()
